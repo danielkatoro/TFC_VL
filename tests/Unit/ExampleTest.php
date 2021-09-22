3604ae81f2427b6c7cc2ae2740044e32550ea8a2
danielkatoro <danielkatoro@gmail.com>
2021-09-22 19:39:15 +0100

    test unitaires

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -11,7 +11,7 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function test_everythings_work_perfecty()
     {
         $this->assertTrue(true);
     }
