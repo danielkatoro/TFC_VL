80acde6d4dff283b2e47cc3c09f0ab15900c0c80
danielkatoro <danielkatoro@gmail.com>
2021-10-06 06:35:10 +0100

    drop projet migration, create dossier one

--- database/migrations/2021_10_06_053434_create_table_dossier.php
+++ database/migrations/2021_10_06_053434_create_table_dossier.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableProjet extends Migration
+class CreateTableDossier extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTableProjet extends Migration
      */
     public function up()
     {
-        Schema::create('table_projet', function (Blueprint $table) {
+        Schema::create('table_dossier', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateTableProjet extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table_projet');
+        Schema::dropIfExists('table_dossier');
     }
 }
